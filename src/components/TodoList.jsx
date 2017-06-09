d90f63b20766c9165548fb69595f5da13a8bb2a3
Adityo Pratomo <kotakmakan@yahoo.com>
2017-06-09 07:40:20 +0700

    todo list item can now be marked as done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 
 import TodoItem from './TodoItem';
+import TodoInput from './TodoInput';
 
 class TodoList extends React.Component {
-    render() {
-        const items = [
-            {name: 'First item', done: false},
-            {name: 'Second item', done: true},
-            {name: 'Third item', done: false}
-        ];
+    constructor() {
+        super();
 
+        this.state = {
+            items: []
+        };
+    }
+
+    handleAddItem(name) {
+        console.log("TodoList handleAddItem", name);
+        const newItems = this.state.items.concat({name: name, done: false});
+        this.setState({items: newItems});
+    }
+
+    handleToggleDone(item) {
+        const newItems = this.state.items.slice();
+        newItems[newItems.indexOf(item)].done = !item.done;
+
+        this.setState({ items: newItems});
+    }
+
+    render() {
         return (
-            <ul>
-                {items.map(item => <TodoItem name={item.name} done={item.done} />)}
-            </ul>
+            <div>
+                <ul>
+                    {this.state.items.map(item => <TodoItem name={item.name} done={item.done} onToggleDone={this.handleToggleDone.bind(this, item)} />)}
+                </ul>
+                <TodoInput onAddItem={this.handleAddItem.bind(this)} />
+            </div>
         );
     }
 }
